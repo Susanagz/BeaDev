2a94379967b8141268a37cd6c24bd435bf424e18
Susanagz <62529923+Susanagz@users.noreply.github.com>
2021-09-23 21:57:54 -0500

    cambio beadoc

--- Views/src/Home.vue
+++ Views/src/Home.vue
@@ -8,7 +8,7 @@
     >
       <!-- <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon> -->
       <v-toolbar-title style="width: 300px" class="ml-0 pl-4">
-        <span class="hidden-sm-and-down">BeaDev</span>
+        <span class="hidden-sm-and-down">BeaDoc</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
       <v-tooltip right color="white">
