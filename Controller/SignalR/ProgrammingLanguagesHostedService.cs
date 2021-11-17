9a174fce7bb4c861aecfaadefec148cf43386f98
jobst <jobstuff951127@gmail.com>
2021-11-16 20:47:14 -0600

    Issues fixed when theres no data yet

--- Controller/SignalR/ProgrammingLanguagesHostedService.cs
+++ Controller/SignalR/ProgrammingLanguagesHostedService.cs
@@ -34,75 +34,79 @@ namespace Controller.SignalR
                 programmingLanguage = context.ProgrammingLanguages.ToList();
             }
 
-            var topThree = programmingLanguage.GroupBy(x => x.Name, d => d.SalaryRange)
-                .Where(g => g.Count() > 0)
-                .Select(y => new { Element = y.Key, Counter = y.Count(), SalaryRate = y.Average() })
-                .OrderByDescending(r => r.Counter)
-                .Take(3)
-                .ToList();
-
-            var topThreeSD = programmingLanguage.Where(q => q.Name == topThree[0].Element)
-                .GroupBy(x => x.StackDescription)
-                .Where(g => g.Count() > 1)
-                .Select(y => new { Element = y.Key, Counter = y.Count() })
-                .OrderByDescending(r => r.Counter)
-                .Take(1)
-                .ToList();
-
-            var topThreeSD1 = programmingLanguage.Where(q => q.Name == topThree[1].Element)
-                .GroupBy(x => x.StackDescription)
-                .Where(g => g.Count() > 1)
-                .Select(y => new { Element = y.Key, Counter = y.Count() })
-                .OrderByDescending(r => r.Counter)
-                .Take(1)
-                .ToList();
-
-            var topThreeSD2 = programmingLanguage.Where(q => q.Name == topThree[2].Element)
-                .GroupBy(x => x.StackDescription)
-                .Where(g => g.Count() > 1)
-                .Select(y => new { Element = y.Key, Counter = y.Count() })
-                .OrderByDescending(r => r.Counter)
-                .Take(1)
-                .ToList();
-
-
-            List<dynamic> list = new List<dynamic>();
-
-
-            var top1 = new
+            try
             {
-                Position = 1,
-                Name = topThree[0].Element,
-                Counter = topThree[0].Counter,
-                SalaryRate = topThree[0].SalaryRate,
-                topThreeStackDescList = topThreeSD[0].Element
-            };
+                var topThree = programmingLanguage.GroupBy(x => x.Name, d => d.SalaryRange)
+              .Where(g => g.Count() > 0)
+              .Select(y => new { Element = y.Key, Counter = y.Count(), SalaryRate = y.Average() })
+              .OrderByDescending(r => r.Counter)
+              .Take(3)
+              .ToList();
 
-            var top2 = new
+                var topThreeSD = programmingLanguage.Where(q => q.Name == topThree[0].Element)
+                    .GroupBy(x => x.StackDescription)
+                    .Where(g => g.Count() > 1)
+                    .Select(y => new { Element = y.Key, Counter = y.Count() })
+                    .OrderByDescending(r => r.Counter)
+                    .Take(1)
+                    .ToList();
+
+                var topThreeSD1 = programmingLanguage.Where(q => q.Name == topThree[1].Element)
+                    .GroupBy(x => x.StackDescription)
+                    .Where(g => g.Count() > 1)
+                    .Select(y => new { Element = y.Key, Counter = y.Count() })
+                    .OrderByDescending(r => r.Counter)
+                    .Take(1)
+                    .ToList();
+
+                var topThreeSD2 = programmingLanguage.Where(q => q.Name == topThree[2].Element)
+                    .GroupBy(x => x.StackDescription)
+                    .Where(g => g.Count() > 1)
+                    .Select(y => new { Element = y.Key, Counter = y.Count() })
+                    .OrderByDescending(r => r.Counter)
+                    .Take(1)
+                    .ToList();
+            
+                List<dynamic> list = new List<dynamic>();
+
+                var top1 = new
+                {
+                    Position = 1,
+                    Name = topThree[0].Element,
+                    Counter = topThree[0].Counter,
+                    SalaryRate = topThree[0].SalaryRate,
+                    topThreeStackDescList = topThreeSD[0].Element
+                };
+
+                var top2 = new
+                {
+                    Position = 2,
+                    Name = topThree[1].Element,
+                    Counter = topThree[1].Counter,
+                    SalaryRate = topThree[1].SalaryRate,
+                    topThreeStackDescList = topThreeSD1[0].Element
+                };
+
+                var top3 = new
+                {
+                    Position = 3,
+                    Name = topThree[2].Element,
+                    Counter = topThree[2].Counter,
+                    SalaryRate = topThree[2].SalaryRate,
+                    topThreeStackDescList = topThreeSD2[0].Element
+                };
+
+                list.Add(top1);
+                list.Add(top2);
+                list.Add(top3);
+                _programmingLanguageHub.Clients.All.SendAsync("Receive", list);
+
+            }
+            catch (System.Exception)
             {
-                Position = 2,
-                Name = topThree[1].Element,
-                Counter = topThree[1].Counter,
-                SalaryRate = topThree[1].SalaryRate,
-                topThreeStackDescList = topThreeSD1[0].Element
-            };
-
-            var top3 = new
-            {
-                Position = 3,
-                Name = topThree[2].Element,
-                Counter = topThree[2].Counter,
-                SalaryRate = topThree[2].SalaryRate,
-                topThreeStackDescList = topThreeSD2[0].Element
-            };
-
-            list.Add(top1);
-            list.Add(top2);
-            list.Add(top3);
-
-            _programmingLanguageHub.Clients.All.SendAsync("Receive", list);
+                _programmingLanguageHub.Clients.All.SendAsync("Receive", new List<dynamic>());
+            }
         }
-
         public Task StopAsync(CancellationToken cancellationToken)
         {
             _timer?.Change(Timeout.Infinite, 0);
